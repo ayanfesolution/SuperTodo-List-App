92d47c1b475f9ad16b885365cb0c54e3a58d6c37
Ayanfe Afolabi <afolabi.ayanfe@decagon.dev>
2022-04-19 22:03:19 +0100

    Update to detail page

---
+++ SuperTodoList/SuperTodoList/DetailsTaskViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailsTaskUIViewController.swift
+//  SuperTodoList
+//
+//  Created by Decagon on 19/04/2022.
+//
+
+import UIKit
+
+class DetailsTaskUIViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
